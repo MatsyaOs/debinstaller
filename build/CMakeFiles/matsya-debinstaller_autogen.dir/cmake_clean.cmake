08ba3ec992d7bba4309a3c67e5e04e9834f5c739
Abhishek Sharma <sabhiron5@gmail.com>
2022-05-20 21:51:24 +0530

    ...

--- build/CMakeFiles/matsya-debinstaller_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/matsya-debinstaller_autogen"
-  "matsya-debinstaller_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/matsya-debinstaller_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
